f60d3aea615787cfb50d952888ba1afbab7be993
Md Noble <mdnoble.wd@gmail.com>
2023-11-20 22:48:59 +0600

    added a button on popular menu section

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -3,32 +3,35 @@ import SectionTitle from "../../../components/sectionTitle/SectionTitle";
 import MenuItem from "../../shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
 
-    const [ menu , setMenu ] = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
 
-    useEffect( () => {
-        fetch("menu.json")
-            .then( res => res.json())
-            .then( data =>  {
-                const popularItems = data.filter( item => item.category === 'popular');
-                setMenu(popularItems);
-            })
-    } , [])
-
-    return (
-        <section className="lg:mt-32 mb-20">
-           <SectionTitle
-           heading={"From Our Menu"}
-           subHeading={"Check It Out"}></SectionTitle> 
-           <div className="container mx-auto w-11/12 lg:w-full grid grid-cols-1 md:grid-cols-2 gap-12 mt-12">
-            {
-                menu.map( item => <MenuItem
-                key={item._id} 
-                item={item}></MenuItem>)
-            }
-           </div>
-        </section>
-    );
+  return (
+    <section className="lg:mt-32 mb-20">
+      <SectionTitle
+        heading={"From Our Menu"}
+        subHeading={"Check It Out"}
+      ></SectionTitle>
+      <div className="container mx-auto w-11/12 lg:w-full grid grid-cols-1 md:grid-cols-2 gap-12 mt-12">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="mt-10 text-center">
+        <button className="btn btn-outline text-black uppercase border-0 border-b-4">
+          View Full Menu
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
