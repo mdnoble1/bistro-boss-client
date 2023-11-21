72fafaadb485d59de106698255b25a31a62f20a3
Md Noble <mdnoble.wd@gmail.com>
2023-11-21 11:10:13 +0600

    installed and used parallax on cover

--- src/pages/shared/Cover/Cover.jsx
+++ src/pages/shared/Cover/Cover.jsx
@@ -1,23 +1,27 @@
-const Cover = ( { image , title , description } ) => {
+import { Parallax } from "react-parallax";
+
+const Cover = ({ image, title, description }) => {
   return (
-    <div
-      className="hero h-[300px] lg:h-[500px]"
-      style={{
-        backgroundImage: `url("${image}")`,
-      }}
+    <Parallax
+      blur={{ min: -50, max: 50 }}
+      bgImage={image}
+      bgImageAlt="menu"
+      strength={-200}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-2xl lg:mt-20">
-          <h1 className="mb-5 text-4xl lg:text-5xl font-semibold uppercase cinzel">
-            {title}
-          </h1>
-          <p className="mb-5 uppercase font-semibold">
-            {description}
-          </p>
+      <div
+        className="hero h-[300px] lg:h-[500px]"
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-2xl lg:mt-20">
+            <h1 className="mb-5 text-4xl lg:text-5xl font-semibold uppercase cinzel">
+              {title}
+            </h1>
+            <p className="mb-5 uppercase font-semibold">{description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
