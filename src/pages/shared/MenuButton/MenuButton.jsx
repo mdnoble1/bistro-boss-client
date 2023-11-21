be402450806caf7a8c9f48fe3197922b52c0517e
Md Noble <mdnoble.wd@gmail.com>
2023-11-21 20:12:44 +0600

    updated button style

--- src/pages/shared/MenuButton/MenuButton.jsx
+++ src/pages/shared/MenuButton/MenuButton.jsx
@@ -1,7 +1,7 @@
 const MenuButton = ( { buttonTitle } ) => {
   return (
     <div>
-      <button className="btn btn-outline text-black uppercase border-0 border-b-4">
+      <button className="btn btn-outline text-black uppercase border-0 border-b-4 bg-base-300">
         {buttonTitle}
       </button>
     </div>
