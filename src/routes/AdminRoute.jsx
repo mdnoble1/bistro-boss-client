ab08290b98ef9bf4051b688dafe9bbdec9ae5857
Md Noble <mdnoble.wd@gmail.com>
2023-12-07 22:37:48 +0600

    created adminRoute to make routes more safe

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,27 +1,24 @@
-import PropTypes from "prop-types";
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-
-const PrivateRoute = ({ children }) => {
+const AdminRoute = ({ children }) => {
 
     const { user , loading } = useAuth();
-
+    const [isAdmin, isAdminLoading ] = useAdmin();
     const location = useLocation();
+    
 
-    if (loading){
+
+    if (loading || isAdminLoading){
         return <div className="flex justify-center"><span className="loading loading-spinner loading-lg"></span></div>
     }
 
-    if( user ) {
+    if( user && isAdmin ) {
         return children;
     }
 
     return <Navigate to="/login" state={{from : location }} replace></Navigate>;
 };
 
-export default PrivateRoute;
-
-PrivateRoute.propTypes ={
-    children : PropTypes.node
-}
+export default AdminRoute;
